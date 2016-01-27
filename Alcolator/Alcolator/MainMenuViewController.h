599a6630175e65e7a06a4d2c679718df22db6f63
Donald Dixon <dixondonald@gmail.com>
2016-01-27 16:15:06 -0500

    Checkpoint 21 Navigation Bar completed

---
+++ Alcolator/Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Ziggy on 1/27/16.
+//  Copyright © 2016 Ziggy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
