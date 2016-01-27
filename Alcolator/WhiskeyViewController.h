e43fb382799d4e6e43cc509c3423b05acc77aadb
Donald Dixon <dixondonald@gmail.com>
2016-01-27 12:35:38 -0500

    Checkpoint 20 Connecting Storyboards completed

---
+++ Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Ziggy on 1/27/16.
+//  Copyright © 2016 Ziggy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
