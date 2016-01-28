15de771252874d5901669a8a3b97b006b0513b11
Donald Dixon <dixondonald@gmail.com>
2016-01-28 13:19:18 -0500

    Checkpoint 22 tabbed navigation completed

--- Alcolator/Alcolator/ViewController.m
+++ Alcolator/Alcolator/ViewController.m
@@ -36,6 +36,8 @@
 - (IBAction)sliderDidChange:(UISlider *)sender {
     NSLog(@"Slider value changed to %f", sender.value);
     [self.beerPercentTextField resignFirstResponder];
+    [self.tabBarItem setBadgeValue:[NSString stringWithFormat:@"%d", (int) sender.value]];
+
 }
 
 - (IBAction)buttonPressed:(UIButton *)sender {
